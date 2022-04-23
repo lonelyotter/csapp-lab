dcaad7cc89d4165d513c2f4484b113981afbe0fd
lonelyotter <57280827+lonelyotter@users.noreply.github.com>
2022-04-23 15:11:34 +0800

    finish CacheLab

--- CacheLab/trans.c
+++ CacheLab/trans.c
@@ -24,32 +24,194 @@ void transpose_submit(int M, int N, int A[N][M], int B[M][N])
 {
     if (N == 32)
     {
-        for (int i = 0; i < N; i += 8)
-        {
-            for (int j = 0; j < M; j += 8)
+        int i, j, x1, x2, x3, x4, x5, x6, x7, x8, x, y;
+        for (i = 0; i < N; i += 8)
+            for (j = 0; j < N; j += 8)
             {
-                for (int k = i; k < i + 8; ++k)
+                if (i == j)
                 {
-                    int temp_value0 = A[k][j];
-                    int temp_value1 = A[k][j + 1];
-                    int temp_value2 = A[k][j + 2];
-                    int temp_value3 = A[k][j + 3];
-                    int temp_value4 = A[k][j + 4];
-                    int temp_value5 = A[k][j + 5];
-                    int temp_value6 = A[k][j + 6];
-                    int temp_value7 = A[k][j + 7];
+                    x = i;
+                    x1 = A[x][j];
+                    x2 = A[x][j + 1];
+                    x3 = A[x][j + 2];
+                    x4 = A[x][j + 3];
+                    x5 = A[x][j + 4];
+                    x6 = A[x][j + 5];
+                    x7 = A[x][j + 6];
+                    x8 = A[x][j + 7];
 
-                    B[j][k] = temp_value0;
-                    B[j + 1][k] = temp_value1;
-                    B[j + 2][k] = temp_value2;
-                    B[j + 3][k] = temp_value3;
-                    B[j + 4][k] = temp_value4;
-                    B[j + 5][k] = temp_value5;
-                    B[j + 6][k] = temp_value6;
-                    B[j + 7][k] = temp_value7;
+                    B[x][j] = x1;
+                    B[x][j + 1] = x2;
+                    B[x][j + 2] = x3;
+                    B[x][j + 3] = x4;
+                    B[x][j + 4] = x5;
+                    B[x][j + 5] = x6;
+                    B[x][j + 6] = x7;
+                    B[x][j + 7] = x8;
+
+                    x1 = A[x + 1][j];
+                    x2 = A[x + 1][j + 1];
+                    x3 = A[x + 1][j + 2];
+                    x4 = A[x + 1][j + 3];
+                    x5 = A[x + 1][j + 4];
+                    x6 = A[x + 1][j + 5];
+                    x7 = A[x + 1][j + 6];
+                    x8 = A[x + 1][j + 7];
+
+                    B[x + 1][j] = B[x][j + 1];
+                    B[x][j + 1] = x1;
+
+                    B[x + 1][j + 1] = x2;
+                    B[x + 1][j + 2] = x3;
+                    B[x + 1][j + 3] = x4;
+                    B[x + 1][j + 4] = x5;
+                    B[x + 1][j + 5] = x6;
+                    B[x + 1][j + 6] = x7;
+                    B[x + 1][j + 7] = x8;
+
+                    x1 = A[x + 2][j];
+                    x2 = A[x + 2][j + 1];
+                    x3 = A[x + 2][j + 2];
+                    x4 = A[x + 2][j + 3];
+                    x5 = A[x + 2][j + 4];
+                    x6 = A[x + 2][j + 5];
+                    x7 = A[x + 2][j + 6];
+                    x8 = A[x + 2][j + 7];
+
+                    B[x + 2][j] = B[x][j + 2];
+                    B[x + 2][j + 1] = B[x + 1][j + 2];
+                    B[x][j + 2] = x1;
+                    B[x + 1][j + 2] = x2;
+                    B[x + 2][j + 2] = x3;
+                    B[x + 2][j + 3] = x4;
+                    B[x + 2][j + 4] = x5;
+                    B[x + 2][j + 5] = x6;
+                    B[x + 2][j + 6] = x7;
+                    B[x + 2][j + 7] = x8;
+
+                    x1 = A[x + 3][j];
+                    x2 = A[x + 3][j + 1];
+                    x3 = A[x + 3][j + 2];
+                    x4 = A[x + 3][j + 3];
+                    x5 = A[x + 3][j + 4];
+                    x6 = A[x + 3][j + 5];
+                    x7 = A[x + 3][j + 6];
+                    x8 = A[x + 3][j + 7];
+
+                    B[x + 3][j] = B[x][j + 3];
+                    B[x + 3][j + 1] = B[x + 1][j + 3];
+                    B[x + 3][j + 2] = B[x + 2][j + 3];
+                    B[x][j + 3] = x1;
+                    B[x + 1][j + 3] = x2;
+                    B[x + 2][j + 3] = x3;
+                    B[x + 3][j + 3] = x4;
+                    B[x + 3][j + 4] = x5;
+                    B[x + 3][j + 5] = x6;
+                    B[x + 3][j + 6] = x7;
+                    B[x + 3][j + 7] = x8;
+
+                    x1 = A[x + 4][j];
+                    x2 = A[x + 4][j + 1];
+                    x3 = A[x + 4][j + 2];
+                    x4 = A[x + 4][j + 3];
+                    x5 = A[x + 4][j + 4];
+                    x6 = A[x + 4][j + 5];
+                    x7 = A[x + 4][j + 6];
+                    x8 = A[x + 4][j + 7];
+
+                    B[x + 4][j] = B[x][j + 4];
+                    B[x + 4][j + 1] = B[x + 1][j + 4];
+                    B[x + 4][j + 2] = B[x + 2][j + 4];
+                    B[x + 4][j + 3] = B[x + 3][j + 4];
+                    B[x][j + 4] = x1;
+                    B[x + 1][j + 4] = x2;
+                    B[x + 2][j + 4] = x3;
+                    B[x + 3][j + 4] = x4;
+                    B[x + 4][j + 4] = x5;
+                    B[x + 4][j + 5] = x6;
+                    B[x + 4][j + 6] = x7;
+                    B[x + 4][j + 7] = x8;
+
+                    x1 = A[x + 5][j];
+                    x2 = A[x + 5][j + 1];
+                    x3 = A[x + 5][j + 2];
+                    x4 = A[x + 5][j + 3];
+                    x5 = A[x + 5][j + 4];
+                    x6 = A[x + 5][j + 5];
+                    x7 = A[x + 5][j + 6];
+                    x8 = A[x + 5][j + 7];
+
+                    B[x + 5][j] = B[x][j + 5];
+                    B[x + 5][j + 1] = B[x + 1][j + 5];
+                    B[x + 5][j + 2] = B[x + 2][j + 5];
+                    B[x + 5][j + 3] = B[x + 3][j + 5];
+                    B[x + 5][j + 4] = B[x + 4][j + 5];
+                    B[x][j + 5] = x1;
+                    B[x + 1][j + 5] = x2;
+                    B[x + 2][j + 5] = x3;
+                    B[x + 3][j + 5] = x4;
+                    B[x + 4][j + 5] = x5;
+                    B[x + 5][j + 5] = x6;
+                    B[x + 5][j + 6] = x7;
+                    B[x + 5][j + 7] = x8;
+
+                    x1 = A[x + 6][j];
+                    x2 = A[x + 6][j + 1];
+                    x3 = A[x + 6][j + 2];
+                    x4 = A[x + 6][j + 3];
+                    x5 = A[x + 6][j + 4];
+                    x6 = A[x + 6][j + 5];
+                    x7 = A[x + 6][j + 6];
+                    x8 = A[x + 6][j + 7];
+
+                    B[x + 6][j] = B[x][j + 6];
+                    B[x + 6][j + 1] = B[x + 1][j + 6];
+                    B[x + 6][j + 2] = B[x + 2][j + 6];
+                    B[x + 6][j + 3] = B[x + 3][j + 6];
+                    B[x + 6][j + 4] = B[x + 4][j + 6];
+                    B[x + 6][j + 5] = B[x + 5][j + 6];
+                    B[x][j + 6] = x1;
+                    B[x + 1][j + 6] = x2;
+                    B[x + 2][j + 6] = x3;
+                    B[x + 3][j + 6] = x4;
+                    B[x + 4][j + 6] = x5;
+                    B[x + 5][j + 6] = x6;
+                    B[x + 6][j + 6] = x7;
+                    B[x + 6][j + 7] = x8;
+
+                    x1 = A[x + 7][j];
+                    x2 = A[x + 7][j + 1];
+                    x3 = A[x + 7][j + 2];
+                    x4 = A[x + 7][j + 3];
+                    x5 = A[x + 7][j + 4];
+                    x6 = A[x + 7][j + 5];
+                    x7 = A[x + 7][j + 6];
+                    x8 = A[x + 7][j + 7];
+
+                    B[x + 7][j] = B[x][j + 7];
+                    B[x + 7][j + 1] = B[x + 1][j + 7];
+                    B[x + 7][j + 2] = B[x + 2][j + 7];
+                    B[x + 7][j + 3] = B[x + 3][j + 7];
+                    B[x + 7][j + 4] = B[x + 4][j + 7];
+                    B[x + 7][j + 5] = B[x + 5][j + 7];
+                    B[x + 7][j + 6] = B[x + 6][j + 7];
+                    B[x][j + 7] = x1;
+                    B[x + 1][j + 7] = x2;
+                    B[x + 2][j + 7] = x3;
+                    B[x + 3][j + 7] = x4;
+                    B[x + 4][j + 7] = x5;
+                    B[x + 5][j + 7] = x6;
+                    B[x + 6][j + 7] = x7;
+                    B[x + 7][j + 7] = x8;
+                }
+
+                else
+                {
+                    for (x = i; x < (i + 8); ++x)
+                        for (y = j; y < (j + 8); ++y)
+                            B[y][x] = A[x][y];
                 }
             }
-        }
     }
     else if (N == 64)
     {
